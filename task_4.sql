85f8b3c696267a85b636f9ca292939709c635603
EdwardMogoane <digits955@gmail.com>
2024-11-16 12:46:29 +0200

    initial Commit

--- task_4.sql
+++ task_4.sql
@@ -1,8 +1,7 @@
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- Query to get the full description of the books table
-SELECT COLUMN_NAME, 
+  SELECT COLUMN_NAME, 
        COLUMN_TYPE, 
        IS_NULLABLE, 
        COLUMN_DEFAULT, 
@@ -10,5 +9,5 @@ SELECT COLUMN_NAME,
        NUMERIC_PRECISION, 
        NUMERIC_SCALE 
 FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = 'alx_book_store' 
-  AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = DATABASE() 
+  AND TABLE_NAME = 'Books';
