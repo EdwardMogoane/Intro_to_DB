da7ff8a91bff3cd136ed115a4752c84ac3e7f2ee
Edward Mogoane <157527259+EdwardMogoane@users.noreply.github.com>
2024-11-17 10:09:27 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,10 @@
-SELECT 
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    CHARACTER_MAXIMUM_LENGTH,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM 
-    information_schema.columns
-WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'books';
+SELECT COLUMN_NAME, 
+       COLUMN_TYPE, 
+       IS_NULLABLE, 
+       COLUMN_DEFAULT, 
+       CHARACTER_MAXIMUM_LENGTH, 
+       NUMERIC_PRECISION, 
+       NUMERIC_SCALE 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_SCHEMA = 'alx_book_store' 
+AND TABLE_NAME = 'books';
