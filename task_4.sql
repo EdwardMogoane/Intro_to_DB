717f047e23713f6bf6ab64f5f50cf47417cc1200
Edward Mogoane <157527259+EdwardMogoane@users.noreply.github.com>
2024-11-17 09:59:19 +0200

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,12 @@
--- Use the alx_book_store database
-USE alx_book_store;
-
-  SELECT COLUMN_NAME, 
-       COLUMN_TYPE, 
-       IS_NULLABLE, 
-       COLUMN_DEFAULT, 
-       CHARACTER_MAXIMUM_LENGTH, 
-       NUMERIC_PRECISION, 
-       NUMERIC_SCALE 
-FROM INFORMATION_SCHEMA.COLUMNS 
-WHERE TABLE_SCHEMA = DATABASE() 
-  AND TABLE_NAME = 'books';
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    CHARACTER_MAXIMUM_LENGTH,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    information_schema.columns
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
